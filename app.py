063e1b58db46981fb1153638bd61630537b47a54
Jun <kumejun2000@gmail.com>
2023-05-20 14:55:35 +0900

    base

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/shop', methods=['GET'])
+def shop_list():
+    # kintoneからselect
+    return render_template('shop/list.html', name="店名")
+
+@app.route('/shop/details', methods=['GET'])
+def shop_details():
+    return render_template('shop/details.html')
+
+@app.route('/ticket', methods=['GET'])
+def ticket_list():
+    return render_template('ticket/list.html')
+
+@app.route('/ticket/details', methods=['GET'])
+def ticket_details():
+    return render_template('ticket/details.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port="8080")
