2758a560d2d0c29f94008f0fc2f7eabf1a8d401d
Jun <kumejun2000@gmail.com>
2023-05-20 22:52:27 +0900

    kintone周りのプログラム修正

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect
-from service import line_bot, kintone
+from service import line_bot
+from service.kintone import get_kintone_shop, get_kintone_shop_all, get_kintone_coupon, get_kintone_coupon_all
 
 
 app = Flask(__name__, static_folder="../frontend/static", template_folder="../frontend/templates")
@@ -13,26 +14,30 @@ def init():
 @app.route("/shop", methods=["GET"])
 def shop_list():
     # kintoneからselect
-    # line_bot.send_ticket('sample')
-    return render_template("shop/list.html")
+    # line_bot.send_coupon('sample')
+    shops = get_kintone_shop_all()
+    return render_template("shop/list.html", shops=shops)
 
 
 @app.route("/shop/<shop_id>", methods=["GET"])
 def shop_details(shop_id):
     # kintoneからselect id = shop_id
-    return render_template("shop/details.html")
+    shop = get_kintone_shop(shop_id)
+    return render_template("shop/details.html", shop=shop)
 
 
 @app.route("/coupon", methods=["GET"])
-def ticket_list():
+def coupon_list():
     # kintoneからselect
-    return render_template("coupon/list.html")
+    coupons = get_kintone_coupon_all()
+    return render_template("coupon/list.html", coupons=coupons)
 
 
 @app.route("/coupon/<coupon_id>", methods=["GET"])
-def ticket_details(ticket_id):
-    # kintoneからselect id = ticket_id
-    return render_template("coupon/details.html")
+def coupon_details(coupon_id):
+    # kintoneからselect id = coupon_id
+    coupon = get_kintone_shop(coupon_id)
+    return render_template("coupon/details.html", coupon=coupon)
 
 
 if __name__ == "__main__":
--- backend/service/kintone.py
+++ backend/service/kintone.py
@@ -1,31 +1,52 @@
-import requests
 import os
+import requests
+from dotenv import load_dotenv
 
+
+load_dotenv()
 BASE_URL = os.environ.get('KINTONE_BASE_URL')
-API_TOKEN1 = os.environ.get('KINTONE_SHOP_ACCESS_TOKEN')
-API_TOKEN2 = os.environ.get('KINTONE_COUPON_ACCESS_TOKEN')
+BASE_URL_ALL = os.environ.get('KINTONE_BASE_URL_ALL')
+API_TOKEN_SHOP = os.environ.get('KINTONE_SHOP_ACCESS_TOKEN')
+API_TOKEN_COUPON = os.environ.get('KINTONE_COUPON_ACCESS_TOKEN')
 
 
-def get_kintone_shop(api_token, id):
+def get_kintone_shop(id):
     params = {
         "app": 1,
         "id": id,
     }
-    headers = {"X-Cybozu-API-Token": api_token}
+    headers = {"X-Cybozu-API-Token": API_TOKEN_SHOP}
     resp = requests.get(BASE_URL, headers=headers, params=params)
     return resp
 
 
-def get_kintone_coupon(api_token, id):
+def get_kintone_shop_all():
+    headers = {"X-Cybozu-API-Token": API_TOKEN_SHOP}
+    url = BASE_URL_ALL + "?app=1"
+    resp = requests.get(url, headers=headers)
+    return resp
+
+
+def get_kintone_coupon(id):
     params = {
         "app": 2,
         "id": id,
     }
-
-    headers = {"X-Cybozu-API-Token": api_token}
+    headers = {"X-Cybozu-API-Token": API_TOKEN_COUPON}
     resp = requests.get(BASE_URL, headers=headers, params=params)
     return resp
 
 
+def get_kintone_coupon_all():
+    headers = {"X-Cybozu-API-Token": API_TOKEN_COUPON}
+    url = BASE_URL_ALL + "?app=2"
+    resp = requests.get(url, headers=headers)
+    return resp
+
+
 if __name__ == "__main__":
-    RESP = get_kintone_shop(API_TOKEN1, 2)
+    # RESP = get_kintone_shop(2)
+    # RESP = get_kintone_shop_all()
+    # RESP = get_kintone_coupon(1)
+    RESP = get_kintone_coupon_all()
+    print(RESP.text)
