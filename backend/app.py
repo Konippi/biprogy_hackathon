6161c4aae03ef7edd044ac55cb4b0b54b310ac87
Konippi <konippi-114@outlook.jp>
2023-05-21 08:49:47 +0900

    modified backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect
-from service import line_bot
-from service.kintone import get_kintone_shop, get_kintone_shop_all, get_kintone_coupon, get_kintone_coupon_all
+from service import line_bot, kintone
 
 
 app = Flask(__name__, static_folder="../frontend/static", template_folder="../frontend/templates")
@@ -13,30 +12,26 @@ def init():
 
 @app.route("/shop", methods=["GET"])
 def shop_list():
-    # kintoneからselect
     # line_bot.send_coupon('sample')
-    shops = get_kintone_shop_all()
+    shops = kintone.get_kintone_shop_all()
     return render_template("shop/list.html", shops=shops)
 
 
 @app.route("/shop/<shop_id>", methods=["GET"])
 def shop_details(shop_id):
-    # kintoneからselect id = shop_id
-    shop = get_kintone_shop(shop_id)
+    shop = kintone.get_kintone_shop(shop_id)
     return render_template("shop/details.html", shop=shop)
 
 
 @app.route("/coupon", methods=["GET"])
 def coupon_list():
-    # kintoneからselect
-    coupons = get_kintone_coupon_all()
+    coupons = kintone.get_kintone_coupon_all()
     return render_template("coupon/list.html", coupons=coupons)
 
 
 @app.route("/coupon/<coupon_id>", methods=["GET"])
 def coupon_details(coupon_id):
-    # kintoneからselect id = coupon_id
-    coupon = get_kintone_shop(coupon_id)
+    coupon = kintone.get_kintone_shop(coupon_id)
     return render_template("coupon/details.html", coupon=coupon)
 
 
